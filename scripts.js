09520128d778e662af4adbd437b34f7a14134039
LuisPinto9 <luisenriquepinto9@gmail.com>
2025-05-14 20:53:16 -0500

    Arreglos de interfaz

--- scripts.js
+++ scripts.js
@@ -3,20 +3,42 @@ const chatWindow = document.getElementById("chatWindow");
 const chatBody = document.getElementById("chatBody");
 const sendBtn = document.getElementById("sendBtn");
 const chatInput = document.getElementById("chatInput");
-
-
 const imageInput = document.getElementById("imageInput");
-// const API_URL = "https://fa51-34-10-70-112.ngrok-free.app/test";
-const API_URL = "https://fea0-34-10-70-112.ngrok-free.app/conversar";
+const previewContainer = document.getElementById("previewContainer");
+const previewImage = document.getElementById("previewImage");
+const removeImage = document.getElementById("removeImage");
 
+
+const API_URL = "https://fea0-34-10-70-112.ngrok-free.app/conversar";
 
 chatToggle.addEventListener("click", () => {
   chatWindow.style.display =
     chatWindow.style.display === "flex" ? "none" : "flex";
 });
 
+imageInput.addEventListener("change", () => {
+  const file = imageInput.files[0];
+  if (file && file.type.startsWith("image/")) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      previewImage.src = e.target.result;
+      previewContainer.classList.remove("hidden");
+    };
+    reader.readAsDataURL(file);
+  }
+});
 
-sendBtn.addEventListener("click", async () => {
+function cleanInputs() {
+  imageInput.value = null;
+  previewImage.src = "";
+  previewContainer.classList.add("hidden");
+}
+
+removeImage.addEventListener("click", () => {
+  cleanInputs();
+});
+
+const requestBotAnswer = async () => {
   const mensaje = chatInput.value.trim();
   const imagen = imageInput.files[0];
 
@@ -34,45 +56,57 @@ sendBtn.addEventListener("click", async () => {
   userMsg.className = "user-message";
   userMsg.textContent = mensaje;
   chatBody.appendChild(userMsg);
+  chatBody.scrollTop = chatBody.scrollHeight;
 
   if (imagen) {
     const reader = new FileReader();
     reader.onload = function (e) {
       const img = document.createElement("img");
-      img.src = e.target.result;  // esta es la URL temporal de la imagen
-      img.style.maxWidth = "100px";
+      img.className = "user-message";
+      img.src = e.target.result;
+      img.style.maxWidth = "200px";
       img.style.marginTop = "5px";
       chatBody.appendChild(img);
-
       chatBody.scrollTop = chatBody.scrollHeight;
     };
-    reader.readAsDataURL(imagen);  // convierte el archivo a una URL base64
+    reader.readAsDataURL(imagen);
+    cleanInputs();
   }
 
   try {
     const res = await fetch(API_URL, {
       method: "POST",
-      body: formData
-    })
+      body: formData,
+    });
     const data = await res.json();
-    console.log("hola2", data);
 
     // Mostrar respuesta del bot
     const botMsg = document.createElement("div");
     botMsg.className = "bot-message";
     botMsg.textContent = data.respuesta || "El asistente no respondió.";
     chatBody.appendChild(botMsg);
-  // Mostrar respuesta del bot
-    const predbotMsg = document.createElement("div");
-    predbotMsg.className = "bot-message";
-    predbotMsg.textContent = data.prediccion || "no prediccion disponible.";
-    chatBody.appendChild(predbotMsg);
+    chatBody.scrollTop = chatBody.scrollHeight;
 
-
+    if (data.prediccion !== undefined && data.prediccion !== null && data.prediccion !== "") {
+      const predbotMsg = document.createElement("div");
+      predbotMsg.className = "bot-message";
+      predbotMsg.textContent = data.prediccion || "No hay predicción.";
+      chatBody.appendChild(predbotMsg);
+      chatBody.scrollTop = chatBody.scrollHeight;
+    }
   } catch (error) {
     console.error("Error:", error);
   }
 
   chatInput.value = "";
   imageInput.value = null;
+};
+
+sendBtn.addEventListener("click", requestBotAnswer);
+
+chatInput.addEventListener("keyup", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    requestBotAnswer();
+  }
 });
