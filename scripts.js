de203fce3adbcb93a83ad027f88bc869be64cb3e
LuisPinto9 <luisenriquepinto9@gmail.com>
2025-05-19 17:06:38 -0500

    url

--- scripts.js
+++ scripts.js
@@ -10,7 +10,7 @@ const removeImage = document.getElementById("removeImage");
 const sendBtnWapper = document.getElementById("sendBtnWapper");
 
 const API_URL =
-  "https://back-proyecto-integrador-electiva-ii-850197342974.us-central1.run.app/conversar";
+  "https://back-proyecto-integrador-electiva-ii-850197342974.us-central1.run.app";
 
 chatToggle.addEventListener("click", () => {
   chatWindow.style.display =
